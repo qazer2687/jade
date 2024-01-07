914e10306273211cca67bbb463bfbda61ccfb721
Alex Vasilkovski <alexvasilkosvki@outlook.com>
2024-01-07 17:22:13 +0000

    enable nvidia driver

--- nixos/modules/nvidia/default.nix
+++ nixos/modules/nvidia/default.nix
@@ -6,10 +6,12 @@
   options.modules.nvidia.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.nvidia.enable {
+    services.xserver.videoDrivers = ["nvidia"];
     hardware = {
       nvidia = {
         modesetting.enable = true;
         open = false;
+        nvidiaSettings = true;
       };
       opengl = {
         enable = true;
