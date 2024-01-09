5b6f5aef7143862a9120e4c792916ffd37e4ed6c
Alex Vasilkovski <alexvasilkosvki@outlook.com>
2024-01-09 14:35:37 +0000

    stuff

--- flake.nix
+++ flake.nix
@@ -1,12 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    sops-nix.url = "github:Mic92/sops-nix";
   };
 
   outputs = {
     nixpkgs,
-    sops-nix,
     ...
   } @ inputs: {
     nixosConfigurations = {
@@ -15,8 +13,6 @@
         specialArgs = {inherit inputs;};
         modules = [
           ./nixos/configurations/jade
-          ./nixos/configurations/shared
-          sops-nix.nixosModules.sops
           {
             nix.registry.nixpkgs.flake = nixpkgs;
             nix.nixPath = ["nixpkgs=flake:nixpkgs"];
--- nixos/configurations/jade/default.nix
+++ nixos/configurations/jade/default.nix
@@ -1,4 +1,10 @@
-{...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkDefault;
+in {
   imports = [
     ../../../hardware/jade
     ../../modules
@@ -17,11 +23,8 @@
     };
   };
 
-  services.xserver.desktopManager.gnome.enable = true;
-
   modules = {
     bash.enable = true;
-    foot.enable = true;
     kernel.enable = true;
     networkmanager.enable = true;
     nvidia.enable = true;
@@ -29,5 +32,62 @@
     prismlauncher.enable = true;
     systemd-boot.enable = true;
     zram.enable = true;
+  };
+
+  services.logrotate.enable = false;
+  services.logrotate.checkConfig = false;
+
+  system.stateVersion = mkDefault "23.05";
+
+  nixpkgs.config.allowUnfree = mkDefault true;
+
+  nix.settings = {
+    experimental-features = mkDefault [
+      "nix-command"
+      "flakes"
+    ];
+    keep-derivations = mkDefault true;
+    keep-outputs = mkDefault true;
+    auto-optimise-store = mkDefault true;
+    sandbox = mkDefault true;
+  };
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override {
+      fonts = [
+        "FiraCode"
+        "FiraMono"
+        "Iosevka"
+      ];
+    })
+  ];
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+
+  console.keyMap = "colemak";
+  
+
+  time.timeZone = mkDefault "Europe/London";
+  i18n.defaultLocale = mkDefault "en_GB.UTF-8";
+
+  programs.direnv.enable = true;
+  programs.git.enable = true;
+
+  environment = {
+    defaultPackages = lib.mkForce [];
+    systemPackages = with pkgs; [
+      firefox
+      obsidian
+      vscodium-fhs
+    ];
+    sessionVariables = {
+      NIXPKGS_ALLOW_UNFREE = "1";
+      NIXPKGS_ALLOW_INSECURE = "1";
+    };
   };
 }
